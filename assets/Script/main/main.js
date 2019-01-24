fd5f905c19e9c4a69af8c768ecb0b498ac268f68
lck8989 <1783887127@qq.com>
2019-01-24 09:15:23 +0800

    asdf

--- assets/Script/main/main.js
+++ assets/Script/main/main.js
@@ -8,7 +8,21 @@ cc.Class({
 
     // LIFE-CYCLE CALLBACKS:
 
-    onLoad () {},
+    onLoad () {
+        //获取到设计分辨率
+        let size = this.node.getComponent(cc.Canvas).designResolution;
+        let frameSize = cc.view.getFrameSize();
+        console.log("frameSize is ",frameSize);
+        if(size.width / size.height < frameSize.width / frameSize.height){
+            //设计分辨率的宽高比 < 屏幕分辨率的宽高比
+            console.log("要适配宽度了");
+            cc.view.setResolutionPolicy(cc.ResolutionPolicy.FIXED_WIDTH);
+        }else{
+            console.log("要适配高度了");
+            cc.view.setResolutionPolicy(cc.ResolutionPolicy.FIXED_HEIGHT);
+        }
+        let sysDesign = cc.view.getResolutionPolicy();
+    },
 
     start () {
 
